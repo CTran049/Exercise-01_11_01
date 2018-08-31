c527a408bc540bf80f22e8eafe6ba8073df7d133
Unknown <ctran049@west-mec.org>
2018-08-31 15:12:29 -0700

    Final Commit

--- solar.php
+++ solar.php
@@ -1,5 +1,5 @@
 <?php
-$WeatherSource = "https://api.forecast.io/forecast/apikey/" . $_GET["lat"] . "," . $_GET["lng"];
+$WeatherSource = "https://api.forecast.io/forecast/3dd2f5a10671febe44f255944b948805/" . $_GET["lat"] . "," . $_GET["lng"];
 header("Content-Type: application/json");
 header("Cache-Control: no-cache");
 readfile($WeatherSource);
